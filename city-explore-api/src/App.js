5fdc16a27295dec587d7feb13d968add82428a51
Eric Starks <123973291+EricDStarks@users.noreply.github.com>
2023-05-18 09:37:13 -0500

    js

--- city-explore-api/src/App.js
+++ city-explore-api/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import FindCity from './Form';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+      <h1>Where In The World!</h1>
+      <FindCity />
       </header>
     </div>
   );
