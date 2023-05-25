755a4f0a96a97cfbbe9d4fb0db1ba8ccf2de78d9
Eric Starks <123973291+EricDStarks@users.noreply.github.com>
2023-05-25 13:02:16 -0500

    js

--- Server.js
+++ Server.js
@@ -1,6 +1,9 @@
+require("dotenv").config()
+
 const express = require("express")
 const cors = require("cors")
 const app = express()
+const axios =  require('axios')
 const WeatherData = require('./Data/Weather.json') 
 class ForeCast {
     constructor(date, description) {
@@ -13,28 +16,30 @@ app.use(cors())
 
 
 app.get('/', function(request, response){
-    response.send("hello")
+    //Send request to weatherbit api
+   
+    response.send(WeatherData)
 })
 //http://http://localhost:3001/Weather?lat=4&lon=4&searchQuery=Paris
-app.get("/Weather", function(request, response) {
-   
-
-    console.log(request.query.lat)
-    console.log(request.query.lon)
-    console.log(request.query.searchQuery)
-    let cityData = WeatherData.find(function(element){
-        if(element.city_name === request.query.searchQuery){
-            return true
-
-        }else{
-            return false
-        }
-    })
-    console.log(cityData)
-    let forecastData = cityData.data.map(function(element){
+app.get("/weather", async function(request, response) {
+    //Send request to weatherbit api
+    let cityData = await axios.get(`http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&city=${request.query.searchQuery}`)
+    let forecastData = cityData.data.data.map(function(element){
         return new ForeCast(element.datetime, element.weather.description)
     })
     response.send(forecastData)
+})
+
+let headers = {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDY5NWM2OWRhOTU0MTE0NzE0OTUzMDJlMzQ0NWYyZSIsInN1YiI6IjY0NmUyOTY0NTFlNmFiMDExZGI1MzM4YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dGAgL3gNxjOWY5_Sf_jvQVFdnm-dN-7RwJGcTeTz1GA'
+  }
+app.get("/movies", async function (request, response) {
+    let movie = request.query.movie
+    //send a request moviedb api
+    //URL: https://api.themoviedb.org/3/movie
+    let movieResponse = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${movie}`, headers=headers)
+    response.send(movieResponse.data.results)
 })
 
 app.use((err, req, res, next) => {
