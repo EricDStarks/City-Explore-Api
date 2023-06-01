d11eab7097a92d460e5cb4276339dc3bb1feebba
Eric Starks <123973291+EricDStarks@users.noreply.github.com>
2023-06-01 10:38:32 -0500

    js

--- src/Movies.js
+++ src/Movies.js
@@ -1,6 +1,7 @@
 
 const axios = require('axios')
 
+let moviecache = {}
 
 let headers = {
     accept: 'application/json',
@@ -8,10 +9,18 @@ let headers = {
 }
 
 async function getMovies(city) {
+    // check cache for data
+    let movieResponse = moviecache[city]
+
+    if (movieResponse == undefined) {
+        movieResponse = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}`, headers = headers)
+        moviecache[city] = movieResponse.data
+        console.log("cache miss")
+    }
 
     //send a request mov iedb api
     //URL: https://api.themoviedb.org/3/movie
-    let movieResponse = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}`, headers = headers)
+   
     return (
         movieResponse.data.results
     )
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 
 const axios =  require('axios')
 
+let weathercache = {}
 
 class ForeCast {
     constructor(date, description) {
@@ -9,7 +10,15 @@ class ForeCast {
     }
 }
 async function getWeather(city) {
-    let cityData = await axios.get(`http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&city=${city}`)
+    //check caxhe for data
+    let cityData = weathercache[city]
+
+    if (cityData == undefined){
+        cityData = await axios.get(`http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&city=${city}`)
+        weathercache[city] = cityData
+        console.log ("cache miss")
+    }
+    
     let forecastData = cityData.data.data.map(function(element){
         return new ForeCast(element.datetime, element.weather.description)
     })
